f47bb14209d73bd608561fc6c4c714ed744cd35f
nicksan29 <nicolasandolifeitosa@gmail.com>
2022-05-19 19:50:02 -0300

    alt1

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Seri;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class SerieController extends Controller
 {
@@ -19,17 +21,19 @@ class SerieController extends Controller
     }
 
     public function store(Request $request){
-        dd($request->all());
-        $request->validate([
-            "name" => "string|required",
-            "seasons" => "string|required",
-            "gen" => "string|required",
-        ]);
+        if ($request->hasFile('image') && $request->file('image')->isValid()){
+            $image=$request->image;
+            $ext=$image->extension();
+            $imagename=$image->getClientOriginalName() . strtotime("now") . "." . $ext;
+///           $image->storeAs("series", $imagename);
+            Storage::disk("public")->put("series/" . $imagename, $image);
+        }
         
         $seri= Seri::create([
             "name" => $request->name,
             "seasons" => $request->seasons,
             "gen" => $request->gen,
+            "image" => $imagename,
         ]);
         return redirect(route("seri.index"))->withSuccess("série salva");
     }
