08a0cd07ab3b16dd9024a940743b2588b61ae80e
nicksan29 <nicolasandolifeitosa@gmail.com>
2022-06-27 19:54:56 -0300

    alteracoes

--- resources/views/seri-index.blade.php
+++ resources/views/seri-index.blade.php
@@ -1,6 +1,6 @@
 @include("parts.header")
 <section class="container-fluid" id="Cont1">
-    <div class="row">
+
         <div class="container">
             <div class="row">
                 <a href="{{route("seri.create")}}">criar</a>
@@ -14,12 +14,12 @@
                                 <th>Ver</th> --}}
                             {{-- </tr> --}}
                             @foreach($seri as $s)
-                                <div class="col-12">
-                                    <tr>
-                                        <td class="pou">{{ $s->name }}</td>
+                                <div class="col-4 mx-auto">
+                                    <div class="card">
+                                        <img src="/storage/series/{{ $s->image }}" alt="">
+                                        <h2>{{ $s->name }}</h2>
                                         {{-- <td>{{$s->seasons}}</td>
                                         <td>{{ $s->gen }}</td> --}}
-                                        <td><img src="/storage/series/{{ $s->image }}" alt=""></td>
                                         <td><a href="{{route("seri.edit" , $s->id)}}">Editar</a></td>
                                         <td><form action="{{route("seri.delete", $s->id)}}" method="POST">
                                         @csrf
@@ -29,11 +29,10 @@
                                         <td><form action="{{route("seri.bio", $s->id)}}">
                                         <button type="submit" >Ver</button>
                                         </form></td>
-                                    </tr>
+                                </div>
                                 </div>
                             @endforeach
             </div>
         </div>
-    </div>
 </section>
 @include("parts.footer")
